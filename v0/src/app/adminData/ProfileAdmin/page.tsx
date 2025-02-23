bf7172f593263357c43993d095e937e856928805
SKH-TMS <k.s.h.taskmanagement@gmail.com>
2025-02-23 22:16:14 +0500

    Add files via upload

--- v0/src/app/adminData/ProfileAdmin/page.tsx
+++ v0/src/app/adminData/ProfileAdmin/page.tsx
@@ -104,23 +104,7 @@ export default function ProfileAdmin() {
     }
   };
 
-  const handleLogout = async () => {
-    sessionStorage.removeItem("userType"); // Clear userType from storage
-    router.push("/adminData/LoginAdmin"); // Redirect to AdminLogin
-
-    try {
-      const response = await fetch("../../api/auth/logout", {
-        method: "GET",
-      });
-      const data = await response.json();
-      if (!data.success) {
-        console.error("Error logging out:", data.message);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
-
+  
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -169,12 +153,7 @@ export default function ProfileAdmin() {
         >
           Change Profile Picture
         </button>
-        <button
-          onClick={handleLogout}
-          className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
-        >
-          Log Out
-        </button>
+        
       </div>
     </div>
   );
